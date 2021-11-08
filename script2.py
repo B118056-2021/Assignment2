c2facf8828222cb9b8114c3ce6a885abfa311cb6
SalomeBrunon <s1704661@ed.ac.uk>
2021-11-08 15:08:22 +0000

    Built the command

--- script2.py
+++ script2.py
@@ -3,7 +3,7 @@ import os, subprocess
 os.system('ls')
 taxon = input("What is your taxon of interest\n\t> ")
 protein_family=input("What is the protein family\n\t> ")
-mycommand="esearch -db protein -query 'Aves[organism] AND Glucose-6-phosphatase[Protein name] NOT PARTIAL' | efetch -db protein -format fasta > {}.fa ".format(taxon, protein_family, taxon)
+mycommand="esearch -db protein -query '{}[organism] AND {}[Protein name] NOT PARTIAL' | efetch -db protein -format fasta > {}.fa ".format(taxon, protein_family, taxon)
 
 
 os.system(mycommand)
