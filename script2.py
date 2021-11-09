4dec7130f4e488f358b509d760c8a666c680ad83
SalomeBrunon <s1704661@ed.ac.uk>
2021-11-09 18:06:16 +0000

    Aligned the protein sequences using aligncopy tool from EMBOSS

--- script2.py
+++ script2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import os, subprocess
-os.system('ls')
 taxon = input("What is your taxon of interest\n\t> ")
 protein_family=input("What is the protein family\n\t> ")
-mycommand="esearch -db protein -query '{}[organism] AND {}[Protein name] NOT PARTIAL' | efetch -db protein -format fasta > {}.fa ".format(taxon, protein_family, taxon)
+esearch_command="esearch -db protein -query '{}[organism] AND {}[Protein name] NOT PARTIAL' | efetch -db protein -format fasta > {}_esearch.fa ".format(taxon, protein_family, taxon)
+os.system(esearch_command)
+subprocess.call(esearch_command, shell=True)
 
-
-os.system(mycommand)
-subprocess.call(mycommand, shell=True)
+align_command="aligncopy -sprotein1 -sequences *_esearch.fa -outfile {}_alignment.fa".format(taxon)
+os.system(align_command)
+subprocess.call(align_command, shell=True)
