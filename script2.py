45c3fdb088be15bb2d110dda6b619f5e97c7a025
SalomeBrunon <s1704661@ed.ac.uk>
2021-11-13 11:34:48 +0000

    Aligned sequences using clustalo instead of aligncopy

--- script2.py
+++ script2.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 import os, subprocess
+
+Glucose-6-phosphatase
+
+#USER INPUT 
 taxon = input("What is your taxon of interest\n\t> ")
 protein_family=input("What is the protein family\n\t> ")
+
+#Esearch using Edirect
 esearch_command="esearch -db protein -query '{}[organism] AND {}[Protein name] NOT PARTIAL' | efetch -db protein -format fasta > {}_esearch.fa ".format(taxon, protein_family, taxon)
 os.system(esearch_command)
 subprocess.call(esearch_command, shell=True)
 
-align_command="aligncopy -sprotein1 -sequences *_esearch.fa -outfile {}_alignment.fa".format(taxon)
-os.system(align_command)
-subprocess.call(align_command, shell=True)
+#Clustalo
+subprocess.call("clustalo -i {}_esearch.fa -o {}_clustalo.fa -v".format(taxon), shell=True)
+
+#Alignment of sequences obtained from edirect search
+#subprocess.call("aligncopy -sprotein1 -sequences *_esearch.fa -outfile {}_alignment.fa".format(taxon), shell=True)
+
+#Determine and plot level of protein sequence conservation across the species within that taxonomic group
+subprocess.call("plotcon -sprotein1 -sequences {}_clustalo.fa -winsize 10  -graph x11".format(taxon), shell=True)
